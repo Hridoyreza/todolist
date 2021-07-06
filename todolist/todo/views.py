51f8bdfdf4ffbdd819b31a7169cbee5dcda99338
Hridoyreza <rezaurhridoy@gmail.com>
2021-07-07 04:10:05 +0600

    alignment updated

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+
 from .models import Todo
 
 
@@ -8,18 +9,15 @@ def index(request):
     todos = Todo.objects.all()
 
     if request.method == 'POST':
-        new_todo = Todo(
-            title = request.POST['title']
-        )
+        new_todo = Todo(title=request.POST['title'])
         new_todo.save()
         return redirect('/')
 
-    return render(request, 'index.html',{'todos':todos})
+    return render(request, 'index.html', {'todos': todos})
 
-def delete(request,pk):
+
+def delete(request, pk):
     if request.method == 'GET':
         deletable_todo = Todo.objects.get(id=pk)
         deletable_todo.delete()
         return redirect('/')
-
-
